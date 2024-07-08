16b8de4b0b16301053daf4b4e19134afc7c85d89
hafyze <hafizmatqwer@gmail.com>
2024-07-08 17:40:37 +0800

    feat: darkmode, header completed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,19 @@
 <script>
-    import { Alert } from 'flowbite-svelte';
-  </script>
+    import { P, Heading, DarkMode } from 'flowbite-svelte';
+</script>
+ 
+<!-- src/app.html -->
+<body class="bg-white dark:bg-gray-800">
+    <DarkMode btnClass="float-right p-4"/>
+
+    <div class="text-center">
+        <Heading tag="h1" class="m-4" customSize="text-4xl font-extrabold  md:text-5xl lg:text-6xl">
+            Malaysia Car Analysis
+        </Heading>
+        <P class="mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+            Looking on all car makers statistics in Malaysia
+        </P>
+
+    </div>
+</body>
   
-  <div class="p-8">
-    <Alert>
-      <span class="font-medium">Info alert!</span>
-      Change a few things up and try submitting again.
-    </Alert>
-  </div>
