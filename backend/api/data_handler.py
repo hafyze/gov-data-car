3ce00e9ee3010f80a7292a8ebd0b96bade767cf4
hafyze <hafizmatqwer@gmail.com>
2024-07-09 13:57:49 +0800

    data: EDA, Filtering cars done

--- backend/api/data_handler.py
+++ backend/api/data_handler.py
@@ -1,13 +1,48 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-# URL to the Parquet file
-URL_DATA = 'https://storage.data.gov.my/transportation/cars_2024.parquet'
+years = range(2015, 2025)
+dfs = []
 
-# Read the Parquet file into a pandas DataFrame
-df = pd.read_parquet(URL_DATA)
+for year in years:
+    url = f'https://storage.data.gov.my/transportation/cars_{year}.parquet'
+    try:
+        df = pd.read_parquet(url)
+        dfs.append(df)
+        print(f"Data for {year} loaded successfully.")
+    except Exception as e:
+        print(f"Failed to load data for {year}: {e}")
 
-# Convert 'date' column to datetime if it exists
-if 'date' in df.columns:
-    df['date'] = pd.to_datetime(df['date'])
+# Concatenate dataframes (18-23)
+if dfs:
+    df_full = pd.concat(dfs, ignore_index=True)
+    print("Concatenation successful.")
+else:
+    df_full = pd.DataFrame()
+    print("Error in concatenation.")
 
-print(df)
+# Convert Data (25-30)
+if 'date_reg' in df_full.columns:
+    df_full['date_reg'] = pd.to_datetime(df_full['date_reg'])
+
+for column in ['type', 'maker', 'model', 'colour', 'fuel', 'state']:
+    df_full[column] = df_full[column].astype('category')
+
+# Data Overview
+print("\n=Null Data Value=")
+print(df_full.isnull().sum())
+
+# Filter for BMW
+bmw_cars = df[df['maker'] == "BMW"]
+
+# Visualising Top BMW
+plt.figure(figsize=(14, 10))
+sns.countplot(data=bmw_cars, x='model', order=bmw_cars['model'].value_counts().index)
+plt.title('Top BMW Models (2015-2024)')
+plt.xticks(rotation=90)
+plt.show()
+
+
+## Predictive QUESTIONS: 
+#   - 
